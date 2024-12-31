6a1b95d7d18f52a93e1f1a21535aab1b43e70f86
Zhihua Zhu <133460136+zhuazhuzz@users.noreply.github.com>
2024-12-31 16:45:12 -0500

    init commit
    new xcode project

---
+++ ReadersEssential/ReadersEssential/ReadersEssentialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadersEssentialApp.swift
+//  ReadersEssential
+//
+//  Created by Main Admin on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadersEssentialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReadersEssential/ReadersEssentialTests/ReadersEssentialTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReadersEssentialTests.swift
+//  ReadersEssentialTests
+//
+//  Created by Main Admin on 12/31/24.
+//
+
+import Testing
+@testable import ReadersEssential
+
+struct ReadersEssentialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
